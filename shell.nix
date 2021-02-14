867c067e7d27aef3bc020743275ac1c733aae106
Graham Bennett <graham@grahambennett.org>
2021-02-14 23:21:08 +0000

    Use nativeBuildInputs for tools

--- shell.nix
+++ shell.nix
@@ -4,20 +4,27 @@ with python38Packages;
 buildPythonPackage rec {
   name = "smartbox";
   src = ".";
-  propagatedBuildInputs = [ aiohttp
-                            click
-                            flake8
-                            python-socketio
-                            requests
-                            websocket_client
-                            yapf
-                          ];
 
-  checkInputs = [ freezegun
-                  pytest
-                  pytest-asyncio
-                  pytest-mock
-                  pytest-randomly
-                  requests-mock
-                  tox ];
+  nativeBuildInputs = [
+    pytest
+    flake8
+    yapf
+  ];
+
+  propagatedBuildInputs = [
+    aiohttp
+    click
+    python-socketio
+    requests
+    websocket_client
+  ];
+
+  checkInputs = [
+    freezegun
+    pytest-asyncio
+    pytest-mock
+    pytest-randomly
+    requests-mock
+    tox
+  ];
 }
