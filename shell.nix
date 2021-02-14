a495311afb80d47824b0d7a3f42d3fac7910bb88
Graham Bennett <graham@grahambennett.org>
2021-02-14 23:32:38 +0000

    Make sure we use python-socketio 4.x, even in nixpkgs unstable

--- shell.nix
+++ shell.nix
@@ -1,4 +1,17 @@
-with import <nixpkgs> {};
+with import <nixpkgs> {
+  overlays = [
+    ( self: super: rec {
+      python38 = super.python38.override {
+        packageOverrides = pySelf: pySuper: {
+          # make sure we use python-socketio 4.x, even in nixpkgs unstable
+          python-engineio = self.nur.repos.graham33.python-engineio-3;
+          python-socketio = self.nur.repos.graham33.python-socketio-4;
+        };
+      };
+      python38Packages = python38.pkgs;
+    })
+  ];
+};
 with python38Packages;
 
 buildPythonPackage rec {
