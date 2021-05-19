79ead7f1c17ce99e7e560bef534ee35159355375
Graham Bennett <graham@grahambennett.org>
2021-05-19 23:16:13 +0100

    Update pre-commit-check.sh

--- scripts/pre-commit-check.sh
+++ scripts/pre-commit-check.sh
@@ -1,14 +1,23 @@
-#!/usr/bin/env zsh
+#!/bin/sh
 
-function setup_py_version() {
+set -eu
+
+setup_py_version() {
     sed -n -e 's/^.*version="\([0-9\.]\+\)".*$/\1/p' setup.py
 }
 
-function changelog_version() {
+changelog_version() {
     sed -n -e 's/^.*## \([0-9\.]\+\).*$/\1/p' CHANGELOG.md | head -1
 }
-if [[ $(setup_py_version) != $(changelog_version) ]]
+
+if [ $(setup_py_version) != $(changelog_version) ]
 then
     echo "setup.py version $(setup_py_version) does not match changelog $(changelog_version)" >&2
     exit 1
 fi
+
+black --check .
+
+flake8
+
+mypy .
