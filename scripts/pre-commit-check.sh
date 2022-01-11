87c2e6710ceae92e438295d0c7fa68dc0df1e417
Graham Bennett <graham@grahambennett.org>
2022-01-11 20:59:53 +0000

    Add __version__ attribute

--- scripts/pre-commit-check.sh
+++ scripts/pre-commit-check.sh
@@ -6,9 +6,19 @@ setup_cfg_version() {
     sed -n -e 's/^.*version = \([0-9\.]\+\).*$/\1/p' setup.cfg
 }
 
+module_version() {
+    sed -n -e 's/^__version__ = "\([0-9\.]\+\)".*$/\1/p' smartbox/__init__.py
+}
+
 changelog_version() {
     sed -n -e 's/^.*## \([0-9\.]\+\).*$/\1/p' CHANGELOG.md | head -1
 }
+
+if [ "$(setup_cfg_version)" != "$(module_version)" ]
+then
+    echo "setup.cfg version $(setup_cfg_version) does not match module $(module_version)" >&2
+    exit 1
+fi
 
 if [ "$(setup_cfg_version)" != "$(changelog_version)" ]
 then
