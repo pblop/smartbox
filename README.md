93986b8107057b353917ef7e15a57a36db2e2d8f
Graham Bennett <graham@grahambennett.org>
2022-02-05 16:22:29 +0000

    Remove changelog

--- README.md
+++ README.md
@@ -5,6 +5,4 @@ Python API to control heating 'smart boxes'
 **Note: this is very much a work in progress**, a lot of functionality is
 missing or not fully working!
 
-See [CHANGELOG.md](./CHANGELOG.md) for release notes.
-
 See [api-notes.md](./api-notes.md) for notes on REST and socket.io endpoints.
--- scripts/pre-commit-check.sh
+++ scripts/pre-commit-check.sh
@@ -10,19 +10,9 @@ module_version() {
     sed -n -e 's/^__version__ = "\([0-9\.]\+\)".*$/\1/p' smartbox/__init__.py
 }
 
-changelog_version() {
-    sed -n -e 's/^.*## \([0-9\.]\+\).*$/\1/p' CHANGELOG.md | head -1
-}
-
 if [ "$(setup_cfg_version)" != "$(module_version)" ]
 then
     echo "setup.cfg version $(setup_cfg_version) does not match module $(module_version)" >&2
-    exit 1
-fi
-
-if [ "$(setup_cfg_version)" != "$(changelog_version)" ]
-then
-    echo "setup.cfg version $(setup_cfg_version) does not match changelog $(changelog_version)" >&2
     exit 1
 fi
 
