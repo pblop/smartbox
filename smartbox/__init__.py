28f679f658ad019ad4d36b4ef45572b4424b8c97
Graham Bennett <graham@grahambennett.org>
2022-02-05 18:29:24 +0000

    Prepare release 1.0.0

--- smartbox/__init__.py
+++ smartbox/__init__.py
@@ -2,4 +2,4 @@ from .error import SmartboxError  # noqa: F401
 from .session import Session  # noqa: F401
 from .socket import SocketSession  # noqa: F401
 
-__version__ = "0.7.2"
+__version__ = "1.0.0"
