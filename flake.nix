9cd90c68baa5324d775ee131b565735b789639f1
Graham Bennett <graham@grahambennett.org>
2022-10-12 22:37:40 +0100

    Add flakes

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Smartbox python package";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nur.url = "github:nix-community/NUR";
+  };
+
+  outputs = { self, nixpkgs, nur }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [
+        (self: super: {
+          nur = import nur {
+            nurpkgs = self;
+            pkgs = self;
+          };
+        })
+      ];
+    };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      inputsFrom = [
+        pkgs.nur.repos.graham33.home-assistant.python.pkgs.smartbox
+      ];
+      packages = with pkgs; [
+        black
+      ];
+    };
+  };
+}
