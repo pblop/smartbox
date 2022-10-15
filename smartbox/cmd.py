8f52acd219927353a9427cb3f2061becce8e3c8e
Graham Bennett <graham@grahambennett.org>
2022-10-15 13:05:59 +0100

    Add __main__ handling for debugging

--- smartbox/cmd.py
+++ smartbox/cmd.py
@@ -214,3 +214,8 @@ def socket(ctx, device_id):
     event_loop = asyncio.get_event_loop()
     task = event_loop.create_task(socket_session.run())
     event_loop.run_until_complete(task)
+
+
+# For debuggging
+if __name__ == "__main__":
+    smartbox()
