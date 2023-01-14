8b26795d19d385b0d22e0d4822e1604dcebc30a2
Graham Bennett <graham@grahambennett.org>
2023-01-14 19:59:35 +0000

    Add mypy to pre-commit-check.sh

--- scripts/pre-commit-check.sh
+++ scripts/pre-commit-check.sh
@@ -10,10 +10,15 @@ module_version() {
     sed -n -e 's/^__version__ = "\([0-9\.]\+\(-[a-z0-9\.]\+\)\?\)".*$/\1/p' smartbox/__init__.py
 }
 
+echo Checking module and setup.cfg versions match...
 if [ "$(setup_cfg_version)" != "$(module_version)" ]
 then
     echo "setup.cfg version $(setup_cfg_version) does not match module $(module_version)" >&2
     exit 1
 fi
 
+echo Checking black formatting...
 black --check .
+
+echo Checking mypy...
+mypy .
