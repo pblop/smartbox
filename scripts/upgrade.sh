7d6d5a7e510320d9e8cfe9c874c751a1552fc818
Graham Bennett <graham@grahambennett.org>
2023-04-09 15:47:16 +0100

    Fix flake update script

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -12,8 +12,9 @@ graham33_nur_rev=$(curl -s https://raw.githubusercontent.com/nix-community/NUR/$
 echo graham33/nur-packages: $graham33_nur_rev
 
 # Make sure we're using the same nixpkgs as graham33/nur-packages
-nix flake lock --update-input nixpkgs --inputs-from github:graham33/nur-packages/$graham33_nur_rev
-
-# Note: if the above doesn't work, need to get nixpkgs revision from
-# graham33/nur-packages' lock file and use this:
-#nix flake lock --update-input nixpkgs --override-input nixpkgs github:NixOS/nixpkgs/b58639c0bfbd2903d7be938ab7b55f02fb840c97
+# This doesn't work for some reason...
+#nix flake lock --update-input nixpkgs --inputs-from github:graham33/nur-packages/$graham33_nur_rev
+# Get nixpkgs revision from graham33/nur-packages' lock file and use this:
+nur_nixpkgs_rev=$(curl -s https://raw.githubusercontent.com/graham33/nur-packages/$graham33_nur_rev/flake.lock | jq -r '.nodes.nixpkgs.locked.rev')
+echo graham33/nur-packages nixpkgs rev: $nur_nixpkgs_rev
+nix flake lock --update-input nixpkgs --override-input nixpkgs github:NixOS/nixpkgs/$nur_nixpkgs_rev
